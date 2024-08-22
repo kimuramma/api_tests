74eddb7b580a6d1560b8decec06689f994e7db85
muraz <mura.zhakeshov@gmail.com>
2024-08-22 12:46:20 +0500

    first commit

--- conftest.py
+++ conftest.py
@@ -1,11 +1,12 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, set_key
 import requests
 import pytest
 
 load_dotenv()
 
 HOST = "https://fastcash-back.trafficwave.kz"
+ENV_PATH = '.env'
 
 @pytest.fixture(autouse=True, scope="session")
 def get_token():
@@ -19,4 +20,11 @@ def get_token():
         }
     )
     assert response.status_code == 200
+    print(response.status_code, response.json())
+
+    token = response.json().get('access')
+    if token:
+        # Записываем токен в файл .env
+        set_key(ENV_PATH, "TOKEN", token)
+
     print(response.status_code, response.json())
