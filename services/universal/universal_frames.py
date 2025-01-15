67ad93ed915f8a99b39978367809851db07971d0
muraz <mura.zhakeshov@gmail.com>
2025-01-16 01:15:20 +0500

    fix api-client-adapter bag

--- services/universal/universal_frames.py
+++ services/universal/universal_frames.py
@@ -32,13 +32,75 @@ class UniversalFrames:
         model = AuthModel(**response.json())
         return model
 
+    def get_frames_api(self, uuid: UUID):
+        frames_api_url = urljoin(self.endpoints.frames_api, str(uuid))
+        print(frames_api_url)
+        response = requests.get(
+            url=frames_api_url,
+            headers=self.headers.basic
+        )
+        print(response.status_code, response.json())
+        print('Информация для фреймов получена')
+
+    def get_frames_fraud(self, uuid: UUID):
+        self.endpoints.set_uuid(uuid=str(uuid))
+        frames_fraud = self.endpoints.get_frames_fraud()
+
+        print(frames_fraud)
+        response = requests.get(
+            url=frames_fraud,
+            headers=self.headers.basic
+        )
+        print(response.status_code, response.json())
+        print('Get fraud is passed')
+
+    def post_frames_fraud(self, uuid: UUID):
+        self.endpoints.set_uuid(uuid=str(uuid))
+        frames_fraud = self.endpoints.get_frames_fraud()
+
+        print(frames_fraud)
+        response = requests.post(
+            url=frames_fraud,
+            headers=self.headers.basic,
+            json=self.payloads.fraud_payload
+        )
+        print(response.status_code, response.json())
+        print('POST fraud is passed')
+
+    def get_biometry(self, uuid: UUID):
+        self.endpoints.set_uuid(uuid=str(uuid))
+        frames_biometry = self.endpoints.get_frames_fraud()[1]
+
+        print(frames_biometry)
+        response = requests.get(
+            url=frames_biometry,
+            headers=self.headers.basic,
+        )
+        print(response.status_code, response.json())
+        print('GET biometry is passed')
+
+    def post_biometry(self, uuid: UUID):
+        self.endpoints.set_uuid(uuid=str(uuid))
+        frames_biometry = self.endpoints.get_frames_fraud()[1]
+
+        print(frames_biometry)
+        response = requests.post(
+            url=frames_biometry,
+            headers=self.headers.basic,
+            json=self.payloads.mock_biometry
+        )
+        print(response.status_code, response.json())
+        print('POST biometry is passed')
+        assert response.status_code == 200, f"Expected status 200, but got {response.status_code}"
+        response.json()
+
     def get_frames_fraud(self, uuid: UUID):
         fraud_path = "fraud"
         frames_fraud_url = self.endpoints.frames_urls_with_path(uuid, fraud_path)
         print(frames_fraud_url)
         response = requests.get(
             url=frames_fraud_url,
-            headers=self.headers.basic2
+            headers=self.headers.basic
         )
         print(response.status_code, response.json())
         print('Информация для фреймов получена')
@@ -51,7 +113,7 @@ class UniversalFrames:
         print(frames_fraud_url)
         response = requests.post(
             url=frames_fraud_url,
-            headers=self.headers.basic2,
+            headers=self.headers.basic,
             json=self.payloads.post_fraud
         )
         print(response.status_code, response.json())
@@ -65,7 +127,7 @@ class UniversalFrames:
         print(frames_biometry_url)
         response = requests.get(
             url=frames_biometry_url,
-            headers=self.headers.basic2
+            headers=self.headers.basic
         )
         print(response.status_code, response.json())
         print('Данные для биометрий получены')
@@ -78,7 +140,7 @@ class UniversalFrames:
         print(frames_biometry_url)
         response = requests.post(
             url=frames_biometry_url,
-            headers=self.headers.basic2,
+            headers=self.headers.basic,
             json=self.payloads.mock_biometry
         )
         print(response.status_code, response.json())
@@ -92,7 +154,7 @@ class UniversalFrames:
         print(frames_signature_url)
         response = requests.get(
             url=frames_signature_url,
-            headers=self.headers.basic2
+            headers=self.headers.basic
         )
         print(response.status_code, response.json())
         print('Get signatur passed')
@@ -105,7 +167,7 @@ class UniversalFrames:
         print(frames_signature_url)
         response = requests.post(
             url=frames_signature_url,
-            headers=self.headers.basic2,
+            headers=self.headers.basic,
             json=self.payloads.signature
         )
         print(response.status_code, response.json())
